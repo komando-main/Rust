f6b4fad434f2c1b7379e74c50ede910ce7e10c68
komando-main <86236221+komando-main@users.noreply.github.com>
2023-04-28 04:17:00 +0900

    바인더(변수) 타입 지정 방법(접미사) 프린트 타입표현

--- binary_octal_hexadecimal/src/main.rs
+++ binary_octal_hexadecimal/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let three = 0b11;// 0b 접두사 이진수
-    let thirty = 0o36;// 0o 접두사 팔진수
-    let three_hundred = 0x12c;// 0x 접두사 십육진수
+    let three = 0b11;// 0b 접두사 2진수 0~1 01
+    let thirty = 0o36;// 0o 접두사 8진수 0~7 01234567
+    let three_hundred = 0x12c;// 0x 접두사 16진수 0~f 0123456789ABCDEF
 
     println!("base 10: {} {} {}", three, thirty, three_hundred);//10진수로 표현 하라 {}
     println!("base 2: {:b} {:b} {:b}", three, thirty, three_hundred);//2진수로 표현 하라 {:b} b=바이너리 약자
