bad388928f176f63ea21aaaa31eda54ca98dcbb2
komando-main <86236221+komando-main@users.noreply.github.com>
2023-04-29 14:28:01 +0900

    찾고자 하는 단어의 열 찾아서 열의 번호와 문자 출력 v2

--- grep_lite2/src/main.rs
+++ grep_lite2/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     삶이 팍팍하고 실력이 없으니 무시만 당하고 조롱당하고 욕먹고
     뭐 다 내탓이려니 하고 버티는 방법뿐이 없군";
     
-    let mut line_num:usize = 1;
+    let mut line_num:usize = 1; //문장 1줄을 표시할때 1로 초기화 한다 컴퓨터는 0부터 시작 하기때문이다
 
     for (i, line) in quote.lines().enumerate(){//.lines()가 반복자를 반환 함으로 이를.enumerate()와 결합할수 있다 (.enumerate() 반복하는 동안 현재 카운트와 요소를 산출하는 반복자!) 0 부터 시작한다
         if line.contains(search_term) {
