ec67aff05f11eb7c8881996c74d7df5bc09ca5d1
komando-main <86236221+komando-main@users.noreply.github.com>
2023-04-30 19:42:00 +0900

    for 문을 이용한 문자열 찾기 contains()기본 함수

--- contains/src/main.rs
+++ contains/src/main.rs
@@ -5,7 +5,7 @@ dark square is a picture feverishly turned--in search of what?
 It is the same with book. What do we seek through millions of pages?";
     
 
-
+//for 문을 이용한 문자열 찾기
     for line in quote.lines(){//.lines()가 1라인 씩 반환 한다
         if line.contains(search_term) {//contains() 인자에 찿고자 하는 단어의 라인을 찾아준다.
             
