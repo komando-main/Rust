acc2f4e3702b879f851f636b14f2cf77d86d84c7
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-03 00:32:21 +0900

    벡터에 대한 이해

--- fn_vec/src/main.rs
+++ fn_vec/src/main.rs
@@ -1,16 +1,28 @@
 fn string_collection(st:&str) -> Vec<String> {
     let mut c = vec![];
-    let d = st.is_empty();    
-    while !d {
+    let d = st.is_empty();
+    if !d {
         c.push(st.to_owned());
-        break;
     }
     c
 }
+
+fn str_array(st:&[&str;3])->Vec<String>{
+    let mut c = vec![];
+    // println!("str_array 함수 안 {:?}", st);
+    for a in st.iter() {
+        // println!("{:?}", a.to_string());
+        c.push(a.to_string());
+    }
+    c
+}
+
+
 fn main() {
     let a = ["asd", "qwe", "zxc"];
     let b = string_collection(a[1]);
-    
+    let c = str_array(&a);
     println!("{:?}", b);
     println!("{:?}", a);
+    println!("{:?}", c);
 }
--- fn_vec/target/debug/deps/fn_vec-65ceef1a3adf1e16.d
+++ fn_vec/target/debug/deps/fn_vec-65ceef1a3adf1e16.d
@@ -1,5 +1,5 @@
-c:\kmj\Rust\fn_vec\target\debug\deps\fn_vec-65ceef1a3adf1e16.rmeta: src\main.rs
+C:\kmj\Rust\fn_vec\target\debug\deps\fn_vec-65ceef1a3adf1e16.rmeta: src\main.rs
 
-c:\kmj\Rust\fn_vec\target\debug\deps\fn_vec-65ceef1a3adf1e16.d: src\main.rs
+C:\kmj\Rust\fn_vec\target\debug\deps\fn_vec-65ceef1a3adf1e16.d: src\main.rs
 
 src\main.rs:
