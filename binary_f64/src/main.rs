2350dddda634e6296f325999bb3264ca199e164c
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-05 03:38:49 +0900

    바이너리 f64 에대한 이해

--- binary_f64/src/main.rs
+++ binary_f64/src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let exponent_bits = n_bits << 1;
     let exponent_bits = exponent_bits >> 53; 
     let exponent_bits = exponent_bits << 52; // & 7ff0000000000000
-    let frction_bits = n_bits << 12;
-    let frction_bits = frction_bits >> 12;   // & 000fffffffffffff
-    let combine_bits = sign_bits + exponent_bits + frction_bits;
-    let combine_f64 = f64::from_bits(combine_bits);//비트를 그대로 f64에 표현한다 as f64는 안된다
+    let mantissa_bits = n_bits << 12;
+    let mantissa_bits = mantissa_bits >> 12;   // & 000fffffffffffff
+    let combine_bits = sign_bits + exponent_bits + mantissa_bits;
+    let combine_f64 = f64::from_bits(combine_bits);//비트를 그대로 f64에 표현한다 as f64는 안됨
 
     println!("부호 {:064b}", sign_bits);
     println!("지수 {:064b}", exponent_bits);
-    println!("가수 {:064b}", frction_bits);
+    println!("가수 {:064b}", mantissa_bits);
     println!("\n원본 {:064b}", n_bits);
     println!("결합 {:064b}", combine_bits);
     println!("원본 {}, 결합 {}", n, combine_f64);
