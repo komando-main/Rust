1efe49e0c2250ee78cc13ff8cdcc1a03c9022526
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-04 20:56:43 +0900

    바이너리 이해 2진수

---
+++ binary1/target/debug/binary1.d
@@ -0,0 +1,1 @@
+C:\kmj\Rust\binary1\target\debug\binary1.exe: C:\kmj\Rust\binary1\src\main.rs
---
+++ binary1/target/debug/deps/binary1.d
@@ -0,0 +1,5 @@
+C:\kmj\Rust\binary1\target\debug\deps\binary1.exe: src\main.rs
+
+C:\kmj\Rust\binary1\target\debug\deps\binary1.d: src\main.rs
+
+src\main.rs:
