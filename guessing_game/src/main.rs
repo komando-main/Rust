e186c06313523467bf788838117d67f6e4f4da72
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-05 07:05:43 +0900

    간단한 입출력

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io;//스텐다드 인풋 아웃풋을 사용하기 위한 정의
 
 fn main() {
 
@@ -6,10 +6,10 @@ fn main() {
 
     println!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new();//비어있는 스트링 가변 바인드를 만든다
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");// 키보드로 부터 문자를 입력받아 guess 바인드에 저장하기
 
-    println!("You guessd: {}", guess);
+    println!("You guessd: {}", guess);//저장된 바인드값 출력
 
 }
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\kmj\Rust\guessing_game\target\debug\guessing_game.exe: C:\kmj\Rust\guessing_game\src\main.rs
