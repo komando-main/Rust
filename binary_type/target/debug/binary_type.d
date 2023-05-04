86500b8097801bc85cd8b2d3f972b4bc94526c0d
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-04 22:41:20 +0900

    바인딩타입의 바이너리 이해

---
+++ binary_type/target/debug/binary_type.d
@@ -0,0 +1,1 @@
+C:\kmj\Rust\binary_type\target\debug\binary_type.exe: C:\kmj\Rust\binary_type\src\main.rs
---
+++ binary_type/target/debug/deps/binary_type.d
@@ -0,0 +1,5 @@
+C:\kmj\Rust\binary_type\target\debug\deps\binary_type.exe: src\main.rs
+
+C:\kmj\Rust\binary_type\target\debug\deps\binary_type.d: src\main.rs
+
+src\main.rs:
