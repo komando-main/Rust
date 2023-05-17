00ac62cf3b3ff64f93cb91aeaa83f5b48dacdc7d
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-17 13:16:52 +0900

    스트럭처에 더하기 토큰 사용법

--- generic_token_impl_copy/src/main.rs
+++ generic_token_impl_copy/src/main.rs
@@ -17,7 +17,7 @@ trait Lll<A, B, C> {
     fn get_tupl(&self) -> (&A, &A, &B, &B,&C,&C,f32,f64);
 }
 
-impl<A: Add<Output = A> + Copy, B, C> Lll<A, B, C> for Asd<A, B, C> {//impl블록에 스트럭처에 명시한 A제네릭을 카피도 같이 사용한다고 명시한다 
+impl<A: Add<Output = A> + Copy, B, C> Lll<A, B, C> for Asd<A, B, C> {//impl블록에 스트럭처의 명시한 A제네릭을 ****카피****도 같이 사용한다고 명시한다 
 	fn new(q: A, w: A, e: B, r: B, t: C, y: C) -> Self {
 		 Self {
 			q,//인자 값의 이름이 스트럭처의 바인드 명과 같다면 q:q 라고 명시 할 필요 없다
@@ -30,7 +30,7 @@ impl<A: Add<Output = A> + Copy, B, C> Lll<A, B, C> for Asd<A, B, C> {//impl블�
 	}
 	fn get_list(&self) -> [A; 2] {//카피가 되었기에 넘길수 있다 참조로는 넘길수 없다!
         [self.q + self.w, self.w]// .copy() 가 암묵적임으로 명시 할필요 없다 단 정의는 해줘야 하며 참조로 보내면 안된다
-    }
+	}
 	fn get_vec(&self) -> (Vec<&B>, &str) {
 		(vec![&self.e, &self.r], "잘 할 수 있어")
 	}
