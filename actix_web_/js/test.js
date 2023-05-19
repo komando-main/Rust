1797adc59499473387d3c68d88779d6cedefe022
komando-main <86236221+komando-main@users.noreply.github.com>
2023-05-19 18:56:04 +0900

    러스트 웹서버 기초

--- actix_web_/js/test.js
+++ actix_web_/js/test.js
@@ -1,16 +1,38 @@
 console.log('Hello, World!');
-
+alert("안녕 세상");
 const showPost = (event) => {//인자값에 바인드 이름 명시해 줘야 오류 안나더라
     const cell = event.target;//이밴트 처리
     const text = cell.textContent;//태그에 텍스트를 가저 온다
     console.log(text);
     alert(text);
+    const data = { text: text };
+
+    // POST 요청 보내기
+    fetch('/json', {//  /json 경로로 POST 요청
+        method: 'POST',//'POST'를 지정하여 요청
+        headers: {
+            'Content-Type': 'application/json'// 요청 본문의 타입을 JSON으로 지정
+        },
+        body: JSON.stringify(data)// JSON 형식의 데이터를 지정
+    })
+    .then(response => response.json())//래스폰스 방식을 json 으로 설정한다
+    .then((data) => {
+                console.log(data);
+                return data; //{"text":"클릭 값"} ex){"text":"래몬"} 저장 하면 새로고침해줘야 한다
+                // return text; // "클릭 값" ex)"래몬"
+            }
+        )// 콘솔에 표현
+    .then((data) => {
+        // HTML 요소 생성
+        const mainDiv = document.createElement('div');
+        mainDiv.innerHTML = JSON.stringify(data); //return 위에서 리턴한 방법에따라 div가 위와 동일하게 생성된다 
+
+        // body에 요소 추가
+        document.body.appendChild(mainDiv);//현재 패이지에 추가
+    })
+    .catch(error => console.error(error));
 };
 
-document.querySelectorAll('.hello world').forEach(function(cell) {//클레스 이름 띄어쓰가로 구분 가능 하고 띄어쓰기 전부적으면 중복으로 실행 된다
-    cell.addEventListener('click', showPost);
-});
-
-document.querySelectorAll('.asd').forEach(function(cell) {
-    cell.addEventListener('click', showPost);
+document.querySelectorAll('.hello world').forEach(function(cell) {//클레스 이름 띄어쓰기로 구분 가능
+    cell.addEventListener('click', showPost);//이밴트 사용 1번인자(어떤이밴트인지) 2번인자(뭘 사용 할지)
 });
--- actix_web_/src/main.rs
+++ actix_web_/src/main.rs
@@ -1,25 +1,32 @@
-use actix_web::{App, HttpServer, Result, HttpResponse, get, post, web};
-use actix_files::NamedFile;
-use serde::Deserialize;
-use askama::Template;
+use actix_web::{App, HttpServer, Result, HttpResponse, get, post, web};//서버 만들때
+use actix_files::NamedFile;//html에 CSS JS 적용 하려 할때
+use serde::{Deserialize, Serialize};//포스트 방식의 데이터를 받을때
+use askama::Template;//html 파일에서 for if 등을 사용 하고 싶을때 정의
+#[allow(unused_imports)]
+use serde_json::{json, to_string, Value};//json 방식 사용
 
 #[allow(dead_code)]
-#[derive(Deserialize, Debug)]//post 방식으로 받을때 사용
-struct MyForm {
+#[derive(Deserialize, Serialize, Debug)]//post 방식으로 받을때 사용
+struct MyForm {//묶음 데이터 이기때문에 양식이 틀리면 오류난다
     username: String,
+}
+#[allow(dead_code)]
+#[derive(Deserialize, Serialize, Debug)]//post, json 사용시 설정
+struct FormData {
+    text: String,
 }
 
 #[allow(dead_code)]
-#[derive(Template)]
-#[template(path = "test.html")]//html 에서 if for 등 자동으로 생성 할때 쓰는법 폴더 확인 잘 하자 templates 위에 S 붙어야 한다
+#[derive(Template)]//template을 이용하여 html 파일 사용 설정
+#[template(path = "test.html")]//html 에서 if for 등 자동으로 생성 할때 쓰는법 폴더 확인 잘 하자 templates 뒤에 S 붙어야 한다
 struct List {
     list: Vec<String>,
 }
 
 #[get("/")]//요청 받았을때
-async fn index() -> HttpResponse {//요청 보내 쓰는 리턴 ***** HttpResponse *****
+async fn index() -> HttpResponse {//요청 보내때 쓰는 리턴 ***** HttpResponse *****
     let a = List {//요청 보낼 함수에 직접 데이터 입력 해야 한다
-        list: vec!["사과".to_string(), "바나나".to_string(), "레몬".to_string(), "체리".to_string()],
+        list: vec!["사과".to_owned(), "바나나".to_owned(), "레몬".to_owned(), "체리".to_owned()],
     };
     let rendered_template = a.render().unwrap();//데이터 읽어서 변환하여 보낸다
     HttpResponse::Ok().body(rendered_template)//보낼때 쓰는 함수
@@ -35,16 +42,36 @@ async fn js() -> Result<NamedFile> {
     Ok(NamedFile::open("js/test.js")?)
 }
 
-#[post("/submit")]
-async fn submit(form: web::Form<MyForm>) -> HttpResponse {//포스트 방식으로 받을때 web::Form<MyForm> 이런식으로 해줘야 한다
+#[post("/submit")]//포스트 방식으로 받을때 사용
+async fn submit(form: web::Form<MyForm>) -> HttpResponse {//포스트 방식으로 받을때 인자 값에 web::Form<MyForm> 이런식으로 해줘야 한다
     println!("Received form data: {:?}", form);
-    HttpResponse::Ok().body("Good ^_^/")
+    println!("Received form data: {}", form.username);
+    HttpResponse::Ok().body("Good ^_^/")//다시 웹 패이지로 보넬때
+}
+
+#[post("/json")]
+async fn api_v1(form: web::Json<FormData>) -> HttpResponse {
+    println!("Received form data: {:?}", form);
+    println!("Received form data: {}", form.text);
+    let body = to_string(&form).unwrap();
+    println!("Received form data: {}", body);
+    HttpResponse::Ok().content_type("application/json").body(body.to_string())//content_type()보낼 데이터 유형 설정
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service((index, css, js, submit)))//위에 정의한 함수 이름이 인자 값으로 들어간가
-        .bind("127.0.0.1:8888")?//서버 번호 지정 웹상에 노출하는 방법 내컴퓨터 공인 IP 적어주면 잘된다~!
-        .run()//실행
-        .await
+async fn main() -> std::io::Result<()> {//매인 자채가 in out를 반환 한다
+    HttpServer::new(|| App::new()
+        .service(
+            (//위에 정의한 함수 이름이 인자 값으로 들어간가 함수 만들었으면 여기에 적어라 재발!!
+                index, 
+                css, 
+                js, 
+                submit, 
+                api_v1
+            )
+        )
+    )
+    .bind("127.0.0.1:8888")?//서버 IP 지정 웹상에 노출하는 방법 내컴퓨터 공인 IP 적어주면 잘된다~!
+    .run()//실행
+    .await//비동기 함수 쓸때 사용 (실행 또는 중지) async 비동기 키워드
 }
