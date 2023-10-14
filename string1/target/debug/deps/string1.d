71277db5f1ac8b798b956c33ef0dadc88431bc73
komando-main <86236221+komando-main@users.noreply.github.com>
2023-10-14 23:01:01 +0900

    update

---
+++ string1/target/debug/deps/string1.d
@@ -0,0 +1,5 @@
+C:\kmj\Rust\string1\target\debug\deps\string1.exe: src\main.rs
+
+C:\kmj\Rust\string1\target\debug\deps\string1.d: src\main.rs
+
+src\main.rs:
---
+++ string1/target/debug/string1.d
@@ -0,0 +1,1 @@
+C:\kmj\Rust\string1\target\debug\string1.exe: C:\kmj\Rust\string1\src\main.rs
