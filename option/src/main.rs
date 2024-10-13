8eefb1fe83e952426c4c92f23b042a559530dc0c
komando-main <86236221+komando-main@users.noreply.github.com>
2024-10-13 21:30:23 +0900

    update

--- option/src/main.rs
+++ option/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_mut)]
-fn main() {
+fn main() -> Result<(), String>{
     let mut a = Some(String::from("asd"));
     println!("{}", a.unwrap());
 
@@ -11,31 +11,50 @@ fn main() {
     //                         }
     //                     };
 
-    let out_value = opt(4).unwrap_or_else(|err| {
-                                                            println!("Error: {}", err);  // 오류 메시지를 출력
-                                                            -1  // 대체 값 반환
-                                                        });
+    // let out_value = opt(Some(4)).unwrap_or_else(|err| {
+    //                                                         println!("Error: {}", err);  // 오류 메시지를 출력
+    //                                                         -1  // 대체 값 반환
+    //                                                     });
     // let out_value = opt(4).unwrap_or(0);
+
+    let out_value = opt(Some(4))?;
+
     println!("{out_value}");
-    let out_value = opt(4).unwrap_err();
+    // let out_value = opt(Some(4)).unwrap_or_else(|err| {
+    //     println!("Error: {}", err);  // 오류 메시지를 출력
+    //     -1  // 대체 값 반환
+    // });
+    let out_value = opt(None)?;
     println!("{out_value}");
     // match out_value {
     //     Ok(a)=>println!("{a}"),
     //     Err(a)=>println!("{a}")
     // }
+
+    Ok(())
 }
 
-fn opt(a:i32)->Result<i32, String>{
+fn opt(a:Option<i32>)->Result<i32, String>{
     
-    // if a == 3 {
-    //     Ok(a)
+    // if a != None {
+    //     Ok(a.unwrap())
     // } else {
     //     Err("no data".to_string())
     // }
 
-    match a {
-        3 => {Ok(a)},
-        _ => {Err("no data".to_string())},
-    }
-        
+    // if !a.is_none() {
+    //     Ok(a.unwrap())
+    // } else {
+    //     Err("no data".to_string())
+    // }
+
+    // match a {
+    //     Some(a) => Ok(a),
+    //     _ => Err("no data".to_string()),
+    // }
+    
+    // let num = a.ok_or("no data".to_string())?;
+    // Ok(num)
+
+    Ok(a.ok_or("no data".to_string())?)
 }
